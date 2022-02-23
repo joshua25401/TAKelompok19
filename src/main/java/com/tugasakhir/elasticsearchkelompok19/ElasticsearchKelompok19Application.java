1cba758f441c925c2f6647ee36acd7b4df2eb095
joshua25401 <joshuaryandafrespangaribuan25@gmail.com>
2022-02-23 14:52:04 +0700

    Initiate Project
    Inisiasi Proyek Client

---
+++ src/main/java/com/tugasakhir/elasticsearchkelompok19/ElasticsearchKelompok19Application.java
@@ -0,0 +1,13 @@
+package com.tugasakhir.elasticsearchkelompok19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchKelompok19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchKelompok19Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tugasakhir/elasticsearchkelompok19/ElasticsearchKelompok19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugasakhir.elasticsearchkelompok19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchKelompok19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
